0f73d669175f4a6f9a198f54ca040f43a8432c3f
Michael Duren <michaeld@michaelduren.com>
2023-06-30 07:25:46 -0500

    added join table and updated dbcontext

--- Persistence/DataContext.cs
+++ Persistence/DataContext.cs
@@ -11,5 +11,30 @@ namespace Persistence
         }
 
         public DbSet<Activity> Activities { get; set; }
+        public DbSet<ActivityAttendee> ActivityAttendees { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            // specify primary key by combining the appuserid and activityid    
+            builder.Entity<ActivityAttendee>(x => x.HasKey(aa => new { aa.AppUserId, aa.ActivityId }));
+            
+            // configure the entity itself 
+            // activityattendee has one app user, with app user having many activity attendees,
+            // activity attendee has the foreign key app userid 
+            builder.Entity<ActivityAttendee>()
+                .HasOne(u => u.AppUser)
+                .WithMany(a => a.Activities)
+                .HasForeignKey(aa => aa.AppUserId);
+
+            // the activity attendee has one activity, the activity has many attendees (activityattendees)
+            // the foreign key is activity id
+            builder.Entity<ActivityAttendee>()
+                .HasOne(aa => aa.Activity)
+                .WithMany(a => a.Attendees)
+                .HasForeignKey(aa => aa.ActivityId);
+
+        }
     }
 }
